16696b6deb245cecf847892a0208b364c0979c56
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-01-04 18:39:50 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,27 +2,26 @@ package com.company;
 
 public class Main {
 
-   public static void main(String[] args) {
-       int[]testArr = {8,6,7,5,3,0,9,10,1,2,3};
-       int[]before=testArr;
-   
-       for(int num:testArr){
-           System.out.print(num+" ");
-       }
-       System.out.println();
-       BubbleSort.bubbleSort(testArr);
-      
-       int[]after=testArr;
-       for(int num:testArr){
-           System.out.print(num+" ");// write your code here
-       }
-       System.out.println();
-       long time = System.nanoTime();
-       SortingUtil.bubbleSort(testArr);
-       time = System.nanoTime()-time;
-       System.out.println("Time Taken: "+time);
-       System.out.println("Is it sorted?: "+ SortingUtil.isSorted(testArr));
-       System.out.println("Are the sums equal? :"+SortingUtil.checkSum(before,after));
-   }
-}
+    public static void main(String[] args) {
+        int[]testArr = BubbleSort.randomIntArr(8);
+        int[]before=testArr;
 
+        for(int num:testArr){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        BubbleSort.bubbleSort(testArr);
+
+        int[]after=testArr;
+        for(int num:testArr){
+            System.out.print(num+" ");// write your code here
+        }
+        System.out.println();
+        long time = System.nanoTime();
+        SortingUtil.bubbleSort(testArr);
+        time = System.nanoTime()-time;
+        System.out.println("Time Taken: "+time);
+        System.out.println("Is it sorted?: "+ SortingUtil.isSorted(testArr));
+        System.out.println("Are the sums equal? :"+SortingUtil.checkSum(before,after));
+    }
+}
